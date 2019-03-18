eda5b18a304192ece78ab715da0c14450a7721b4
Steven Tomlinson <t97steven@gmail.com>
2019-03-18 22:47:45 +0000

    Updated main type

--- src/Main.hs
+++ src/Main.hs
@@ -9,6 +9,7 @@ run s = case (pTypeSystem . myLexer) s of
     Ok tree -> (genCode . genIntermediateRepr) tree
     Bad err -> error err
 
+main :: IO ()
 main = do
     contents <- readFile "test/test.jt"
     let rules = run contents
