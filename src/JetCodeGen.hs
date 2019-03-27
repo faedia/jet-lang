5905eb6c2b2fa215fa7405948ce73b6e378daac5
Steven Tomlinson <t97steven@gmail.com>
2019-03-27 01:16:53 +0000

    Expanded pattern match case to catch errors. Also added user defined error handling functions

--- src/JetCodeGen.hs
+++ src/JetCodeGen.hs
@@ -22,6 +22,11 @@ expandConstructor :: (Abs.Ident, [Abs.Ident]) -> String
 expandConstructor (name, []) = id2Str name
 expandConstructor (name, params) = "(" ++ expandConstructor (name, []) ++ " " ++ (init . foldr (\param s -> id2Str param ++ " " ++ s) "") params ++ ")"
 
+wrapInferInCase :: JetMonad -> String
+wrapInferInCase (JInferFunc name ctx var (Abs.TType tname tparams)) 
+    | not (isTypeVar tname) = "case infer" ++ id2Str name ++ " " ++ id2Str var ++ " " ++ h2Str ctx ++ " of Succ jet0@" 
+        ++ expandConstructor (tname, tparams) ++ " -> return jet0; Succ t -> fail (makeInferError " ++ id2Str var ++ " t); x -> x"
+    | otherwise = "infer" ++ id2Str name ++ " " ++ id2Str var ++ " " ++ h2Str ctx
 genSingleMonad :: JetMonad -> String
 genSingleMonad (JSideCond haskell) = h2Str haskell
 genSingleMonad (JCheckFunc count name ctx Abs.TNone var) = 
@@ -30,9 +35,9 @@ genSingleMonad (JCheckFunc count name ctx (Abs.TType tname tparams) var) =
     "var" ++ show count ++ " <- check" ++ id2Str name ++ " " ++ id2Str var ++ " " ++ expandConstructor (tname, tparams) ++ " " ++ h2Str ctx
 genSingleMonad (JInferFunc name ctx var Abs.TNone) = 
     error "Infer functions require a type variable to return to"
-genSingleMonad (JInferFunc name ctx var (Abs.TType tname tparams)) 
+genSingleMonad m@(JInferFunc name ctx var (Abs.TType tname tparams)) 
     | not (null (getTypeVars (Abs.TType tname tparams))) = 
-        expandConstructor (tname, tparams) ++ " <- infer" ++ id2Str name ++ " " ++ id2Str var ++ " " ++ h2Str ctx
+        expandConstructor (tname, tparams) ++ " <- " ++ wrapInferInCase m
     | otherwise = error "Type must be a type variable to call infer"
 
 genMonadCode :: [JetMonad] -> String
@@ -57,7 +62,7 @@ genCheckCode tr (JCheck astName astConst monads (Abs.TType typeName typeParams) 
     "check" ++ id2Str astName ++ " " ++ expandConstructor astConst ++ " jetCheckType ctx = do" ++ nltab 
     ++ (if tr then genTraceCode astName astConst (Abs.TType typeName typeParams) "check" ++ nltab else "") 
     ++ genMonadCode monads
-    ++ "if jetCheckType == " ++ expandConstructor (typeName, typeParams) ++ " then " ++ h2Str ctx ++ " else fail (\"Expected type \" ++ show jetCheckType ++ \" found type \" ++ show " ++ expandConstructor (typeName, typeParams) ++ ")\n"
+    ++ "if jetCheckType == " ++ expandConstructor (typeName, typeParams) ++ " then " ++ h2Str ctx ++ " else fail (makeCheckError " ++ expandConstructor astConst ++ " jetCheckType " ++ expandConstructor (typeName, typeParams) ++ ")\n"
 genCheckCode tr (JCheckListEmpty astName monads Abs.TNone ctx) =
     "check" ++ id2Str astName ++ "List [] ctx = do" ++ nltab
     ++ (if tr then genTraceCode astName (Abs.Ident "", []) Abs.TNone "checkList" ++ nltab else "") 
@@ -77,12 +82,12 @@ genCheckCode tr (JCheckListEmpty astName monads (Abs.TType typeName typeParams) 
     "check" ++ id2Str astName ++ "List [] jetCheckType ctx = do" ++ nltab
     ++ (if tr then genTraceCode astName (Abs.Ident "", []) Abs.TNone "checkList" ++ nltab else "") 
     ++ genMonadCode monads
-    ++ "if jetCheckType == " ++ expandConstructor (typeName, typeParams) ++ " then " ++ h2Str ctx ++ " else fail (\"Expected type \" ++ show jetCheckType ++ \" found type \" ++ show " ++ expandConstructor (typeName, typeParams) ++ ")\n"
+    ++ "if jetCheckType == " ++ expandConstructor (typeName, typeParams) ++ " then " ++ h2Str ctx ++ " else fail (makeCheckErrorList [] jetCheckType " ++ expandConstructor (typeName, typeParams) ++ ")\n"
 genCheckCode tr (JCheckListCons astName item list monads (Abs.TType typeName typeParams) ctx) =
     "check" ++ id2Str astName ++ "List (" ++ id2Str item ++ ":" ++ id2Str list ++ ") jetCheckType ctx = do" ++ nltab
     ++ (if tr then genTraceCode astName (item, [list]) Abs.TNone "checkList" ++ nltab else "") 
     ++ genMonadCode monads
-    ++ "if jetCheckType == " ++ expandConstructor (typeName, typeParams) ++ " then " ++ h2Str ctx ++ " else fail (\"Expected type \" ++ show jetCheckType ++ \" found type \" ++ show " ++ expandConstructor (typeName, typeParams) ++ ")\n"
+    ++ "if jetCheckType == " ++ expandConstructor (typeName, typeParams) ++ " then " ++ h2Str ctx ++ " else fail (makeCheckErrorList (" ++ id2Str item ++ ":" ++ id2Str list ++ ") jetCheckType " ++ expandConstructor (typeName, typeParams) ++ ")\n"
     
 
 genInferCode :: Bool -> JetInfer -> String
